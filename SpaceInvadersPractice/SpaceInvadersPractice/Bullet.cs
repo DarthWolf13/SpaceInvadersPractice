5b4df1eb0b8aaa69346af8cc7ea4a8dbd821d821
DarthWolf13 <martijn.vander.meer@quicknet.nl>
2017-04-10 21:21:15 +0200

    Step 4: Remove Invaders

---
+++ SpaceInvadersPractice/SpaceInvadersPractice/Bullet.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceInvadersPractice
+{
+    class Bullet : SpriteGameObject
+    {
+        Vector2 StartPosition;
+        public Vector2 FirePosition;
+        private int speed = 200;
+
+        public Bullet() : base("bullet")
+        {
+            StartPosition = new Vector2(-100, -100);
+
+            this.Reset();
+        }
+
+        public Bullet(float x, float y) : base("bullet")
+        {
+            this.Position = new Vector2(x, y);
+            this.Velocity = new Vector2(0, -300);
+        }
+
+        public override void Reset()
+        {
+            base.Reset();
+
+            this.Velocity = Vector2.Zero;
+            this.Position = StartPosition;
+        }
+    }
+}
--- SpaceInvadersPractice/SpaceInvadersPractice/PlayingState.cs
+++ SpaceInvadersPractice/SpaceInvadersPractice/PlayingState.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,21 +11,53 @@ namespace SpaceInvadersPractice
     {
         Player ship;
         GameObjectList invaders;
+        GameObjectList bullets;
         private int invaderSize = 9;
 
         public PlayingState()
         {
             ship = new Player();
             invaders = new GameObjectList();
+            bullets = new GameObjectList();
 
             this.Add(ship);
             this.Add(invaders);
+            this.Add(bullets);
 
             for (int i = 0; i < invaderSize; i++)
             {
                 this.invaders.Add(new Invader(0 + 32 * i, 0, "blue_invader"));
                 this.invaders.Add(new Invader(0 + 32 * i, 32, "yellow_invader"));
                 this.invaders.Add(new Invader(0 + 32 * i, 64, "red_invader"));
+            }
+        }
+
+        public override void HandleInput(InputHelper inputHelper)
+        {
+            base.HandleInput(inputHelper);
+
+            if (inputHelper.IsKeyDown(Keys.Space))
+            {
+                this.bullets.Add(new Bullet(ship.Position.X, ship.Position.Y));
+            }
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            foreach (Invader invader in invaders.Objects)
+            {
+                foreach (Bullet bullet in bullets.Objects)
+                {
+                    if (bullet.CollidesWith(invader))
+                    {
+                        bullet.Visible = false;
+                        invader.Visible = false;
+                        //score.ScoreValue += 10;
+                    }
+                }
+
             }
         }
 

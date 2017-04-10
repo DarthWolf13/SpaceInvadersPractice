6e9ccfaf200b9899dc6170d48421fd85d63d709c
DarthWolf13 <martijn.vander.meer@quicknet.nl>
2017-04-10 19:57:38 +0200

    Setup

--- SpaceInvadersPractice/SpaceInvadersPractice/Program.cs
+++ SpaceInvadersPractice/SpaceInvadersPractice/Program.cs
@@ -10,7 +10,7 @@ namespace SpaceInvadersPractice
         /// </summary>
         static void Main(string[] args)
         {
-            using (Game1 game = new Game1())
+            using (SpaceInvaders game = new SpaceInvaders())
             {
                 game.Run();
             }
